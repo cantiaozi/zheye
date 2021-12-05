d2acc1061a45d05b58934c933c6ca46033bac184
liuyong39281 <hspcadmin@WINDOWS-LMCDHD0.hs.handsome.com.cn>
2021-12-05 17:30:13 +0800

    初始化项目

---
+++ src/components/ColumnList.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul v-for="item in list" :key="item.id">
+    <li>
+      <img :src="item.avatar" />
+      <h5>{{item.title}}</h5>
+      <p>{{item.description}}</p>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+export interface ListData {
+  id: number
+  title: string
+  avatar: string
+  description: string
+}
+
+export default defineComponent({
+  name: 'ColumnList',
+  props: {
+    list: {
+      type: Array as PropType<ListData[]>,
+      required: true
+    }
+  }
+})
+</script>
+
+<style></style>
