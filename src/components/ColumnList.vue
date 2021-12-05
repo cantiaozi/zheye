228d6af6a97d022cddced05814b485d599f8677c
liuyong39281 <hspcadmin@WINDOWS-LMCDHD0.hs.handsome.com.cn>
2021-12-05 20:06:12 +0800

    美化ColumnList组件

--- src/components/ColumnList.vue
+++ src/components/ColumnList.vue
@@ -1,15 +1,20 @@
 <template>
-  <ul v-for="item in list" :key="item.id">
-    <li>
-      <img :src="item.avatar" />
-      <h5>{{item.title}}</h5>
-      <p>{{item.description}}</p>
-    </li>
-  </ul>
+   <div class="row">
+    <div v-for="column in columnList" :key="column.id" class="col-4 mb-4">
+      <div class="card h-100 shadow-sm">
+        <div class="card-body text-center">
+          <img  :src="column.avatar" :alt="column.title" class="rounded-circle border border-light w-25 my-3" >
+          <h5 class="card-title">{{column.title}}</h5>
+          <p class="card-text text-left">{{column.description}}</p>
+          <a href="#" class="btn btn-outline-primary">进入专栏</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
+import { computed, defineComponent, PropType } from 'vue'
 export interface ListData {
   id: number
   title: string
@@ -23,6 +28,19 @@ export default defineComponent({
     list: {
       type: Array as PropType<ListData[]>,
       required: true
+    }
+  },
+  setup (props) {
+    const columnList = computed(() => {
+      return props.list.map(item => {
+        if (!item.avatar) {
+          item.avatar = require('@/assets/logo.png')
+        }
+        return item
+      })
+    })
+    return {
+      columnList
     }
   }
 })
