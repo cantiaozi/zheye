10b312e1a11d3a3d61d596e781147b3fe748aaff
liuyong39281 <hspcadmin@WINDOWS-LMCDHD0.hs.handsome.com.cn>
2021-12-23 14:02:51 +0800

    使用axios

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,19 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://apis.imooc.com/api/'
+axios.interceptors.request.use((config) => {
+  config.params = {
+    ...config.params,
+    icode: '8D22E4B7CADF31AE'
+  }
+  return config
+})
+axios.get('/columns?currentPage=1&pageSize=5').then(res => {
+  console.log(res)
+})
 
 const app = createApp(App)
 app.use(store)
