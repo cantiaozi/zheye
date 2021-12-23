62f47df3582562eb221559b054f657cde83764f7
liuyong39281 <hspcadmin@WINDOWS-LMCDHD0.hs.handsome.com.cn>
2021-12-23 13:40:37 +0800

    路由抽离成独立的文件

--- src/main.ts
+++ src/main.ts
@@ -1,53 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { createRouter, createWebHistory, Router } from 'vue-router'
 import store from './store'
-import Home from './views/Home.vue'
-import Login from './views/Login.vue'
-import ColumnDetail from './views/ColumnDetail.vue'
-import CreatePost from './views/CreatePost.vue'
-
-const history = createWebHistory()
-const router: Router = createRouter({
-  history,
-  routes: [
-    {
-      name: 'home',
-      path: '/',
-      component: Home
-    },
-    {
-      name: 'login',
-      path: '/login',
-      component: Login,
-      meta: {
-        redirectAlreadyLogin: true
-      }
-    },
-    {
-      name: 'column',
-      path: '/column/:id',
-      component: ColumnDetail
-    },
-    {
-      path: '/create',
-      name: 'create',
-      component: CreatePost,
-      meta: {
-        requiredLogin: true
-      }
-    }
-  ]
-})
-router.beforeEach((to, from, next) => {
-  if (to.meta.requiredLogin && !store.state.user.isLogin) {
-    next('/login')
-  } else if (to.meta.redirectAlreadyLogin && store.state.user.isLogin) {
-    next('/')
-  } else {
-    next()
-  }
-})
+import router from './router'
 
 const app = createApp(App)
 app.use(store)
---
+++ src/router.ts
@@ -0,0 +1,50 @@
+import { createRouter, createWebHistory, Router } from 'vue-router'
+import Home from './views/Home.vue'
+import Login from './views/Login.vue'
+import ColumnDetail from './views/ColumnDetail.vue'
+import CreatePost from './views/CreatePost.vue'
+import store from './store'
+
+const history = createWebHistory()
+const router: Router = createRouter({
+  history,
+  routes: [
+    {
+      name: 'home',
+      path: '/',
+      component: Home
+    },
+    {
+      name: 'login',
+      path: '/login',
+      component: Login,
+      meta: {
+        redirectAlreadyLogin: true
+      }
+    },
+    {
+      name: 'column',
+      path: '/column/:id',
+      component: ColumnDetail
+    },
+    {
+      path: '/create',
+      name: 'create',
+      component: CreatePost,
+      meta: {
+        requiredLogin: true
+      }
+    }
+  ]
+})
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiredLogin && !store.state.user.isLogin) {
+    next('/login')
+  } else if (to.meta.redirectAlreadyLogin && store.state.user.isLogin) {
+    next('/')
+  } else {
+    next()
+  }
+})
+
+export default router
